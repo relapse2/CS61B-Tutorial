5388ab2086c4327afa8f40e1216a1e285df96564
relapse2 <1329618460@qq.com>
2023-03-30 15:39:13 +0800

    lab6 done

--- lab6/capers/Main.java
+++ lab6/capers/Main.java
@@ -1,6 +1,7 @@
 package capers;
 
 import java.io.File;
+import java.util.Arrays;
 
 import static capers.Utils.*;
 
@@ -41,6 +42,7 @@ public class Main {
             Utils.exitWithError("Must have at least one argument");
         }
 
+
         CapersRepository.setupPersistence();
         String text;
         switch (args[0]) {
@@ -53,10 +55,15 @@ public class Main {
         case "dog":
             validateNumArgs("dog", args, 4);
             // TODO: make a dog
+            String dogName = args[1];
+            String dogBreed = args[2];
+            int dogAge = Integer.parseInt(args[3]);
+            CapersRepository.makeDog(dogName, dogBreed, dogAge);
             break;
         case "birthday":
             validateNumArgs("birthday", args, 2);
             // TODO: celebrate this dog's birthday
+            CapersRepository.celebrateBirthday(args[1]);
             break;
         default:
             exitWithError(String.format("Unknown command: %s", args[0]));
